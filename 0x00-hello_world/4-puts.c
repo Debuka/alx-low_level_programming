00a3e136c9d5862c1cff7efa5e6ce62766087aeb
Michael Chukwuebuka <bukasondean@gmail.com>
2023-02-16 16:05:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A programmee that print a line with put function
+ * Return: o (success)
+ */
+int main(void)
+{
+puts("/"Programming is like building a multilingual puzzle");
+return (0);
+}
