c334bfe8e9e63de8ee0e69e88202706b56e8bd1d
Michael Chukwuebuka <bukasondean@gmail.com>
2023-02-24 18:23:51 -0800

    100-prime_factor.

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -4,22 +4,21 @@
  * print_numbers - prints an integer
  * @n: integer to be printed
  */
-void print_number(int n)
+void print_numbers(int n)
 {
-	unsigned int nl;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		nl = -n;
-		_putchar('_');
+		n1 = -n;
+		_putchar('-');
 	} else
 	{
-		nl = n;
+		n1 = n;
 	}
-
-	if (nl / 10)
-	{
-		print_number(nl / 10);
-	}
-	_putchar((nl % 10) + '0');
+	  if (n1 / 10)
+	  {
+		  print_number(n1 / 10);
+	  }
+	  _putchar((n1 % 10) + '0');
 }
