7f56149aabafc9527252b31d1001655859252f72
debuka <bukasondean@gmail.com>
2023-08-29 15:22:16 -0700

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,7 +13,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
-	listint_t *temp = *head;
+	listint_t *n_n = *head;
 
 	new = malloc(sizeof(listint_t));
 	if (!new)
@@ -28,10 +28,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (new);
 	}
 
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new;
+	while (n_n->next)
+	       n_n = temp->next;
+	
+	n_n->next = new;
 
 	return (new);
 }
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,12 +6,12 @@
  */
 void free_listint(listint_t *head)
 {
-	listint_t *temp;
+	listint_t *n_node;
 
 	while (head)
 	{
-		temp = head->next;
+		n_node = head->next;
 		free(head);
-		head = temp;
+		head = n_node;
 	}
 }
