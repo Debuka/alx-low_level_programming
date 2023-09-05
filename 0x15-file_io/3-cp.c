da5319bd5bc9d5001711d0689cca3f0d8d67bc3c
debuka <bukasondean@gmail.com>
2023-09-05 13:23:39 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,49 +1,10 @@
 #include "main.h"
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 #define BUFFER_SIZE 1024
-
-/**
- * read_textfile - Reads and prints the content of a file.
- * @filename: The name of the file to read.
- * @letters: The number of letters to read and print.
- *
- * Return: The actual number of letters read and printed.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-    int fd;
-    ssize_t bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
-
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-        exit(98);
-    }
-
-    bytes_read = read(fd, buffer, BUFFER_SIZE);
-    if (bytes_read == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-        close(fd);
-        exit(98);
-    }
-
-    bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1)
-    {
-        close(fd);
-        dprintf(STDERR_FILENO, "Error: Can't write to STDOUT\n");
-        exit(99);
-    }
-
-    close(fd);
-    return bytes_written;
-}
 
 /**
  * create_and_copy_file - Copies the content of one file to another.
@@ -56,20 +17,29 @@ int create_and_copy_file(const char *file_from, const char *file_to)
 {
     int fd_from, fd_to;
     ssize_t bytes_read, bytes_written;
-    char buffer[BUFFER_SIZE];
+    char *buffer;
+
+    buffer = malloc(BUFFER_SIZE);
+    if (buffer == NULL)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't allocate buffer\n");
+        exit(99);
+    }
 
     fd_from = open(file_from, O_RDONLY);
     if (fd_from == -1)
     {
         dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        free(buffer);
         exit(98);
     }
 
     fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
     if (fd_to == -1)
     {
-        close(fd_from);
         dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+        free(buffer);
+        close(fd_from);
         exit(99);
     }
 
@@ -78,35 +48,25 @@ int create_and_copy_file(const char *file_from, const char *file_to)
         bytes_written = write(fd_to, buffer, bytes_read);
         if (bytes_written == -1)
         {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+            free(buffer);
             close(fd_from);
             close(fd_to);
-            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
             exit(99);
         }
     }
 
     if (bytes_read == -1)
     {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        free(buffer);
         close(fd_from);
         close(fd_to);
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
         exit(98);
     }
 
+    free(buffer);
     close(fd_from);
     close(fd_to);
-    return (0);
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
-    create_and_copy_file(argv[1], argv[2]);
-
     return (0);
 }
